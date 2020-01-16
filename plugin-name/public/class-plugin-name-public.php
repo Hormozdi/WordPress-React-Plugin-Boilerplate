648887606013fb6bd6e13530bf599867189eba54
hormozdi <hormozdi.hooman@gmail.com>
2020-01-16 21:07:55 +0330

    Load webpack output

--- plugin-name/public/class-plugin-name-public.php
+++ plugin-name/public/class-plugin-name-public.php
@@ -96,6 +96,7 @@ class Plugin_Name_Public {
 		 * class.
 		 */
 
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/react-public.js'      , array( 'jquery' ), $this->version, false );
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-public.js', array( 'jquery' ), $this->version, false );
 
 	}
