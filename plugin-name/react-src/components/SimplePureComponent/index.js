e9655b7c47cbc56fbcff97077efb981e51a1fd01
hormozdi <hormozdi.hooman@gmail.com>
2020-01-17 12:38:07 +0330

    Add simple pure component(Just for test)

---
+++ plugin-name/react-src/components/SimplePureComponent/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class SimplePureComponent extends React.PureComponent {
+  render() {
+    return (
+      <>
+        <h1>Hello Dear Developer</h1>
+        <h2>Start editing to see some magic happen!!!</h2>
+      </>
+    );
+  }
+}
