48b6e6c468c96d145091b5da74a42010a74a45f0
hormozdi <hormozdi.hooman@gmail.com>
2020-01-17 12:43:04 +0330

    Create Base structure for admin part of plugin with ajaxurl for get data from backend

---
+++ plugin-name/react-src/admin.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+jQuery(document).ready(function() {
+  let tempElement = null;
+});
+
+// Set ajax url to get data from Backend
+if (ajaxurl) {
+  window.reactAjaxUrl = ajaxurl;
+}
